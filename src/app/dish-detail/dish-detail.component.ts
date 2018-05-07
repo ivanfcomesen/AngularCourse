701090fff7c260dcd28c1ef1d0a0f34e3b807dff
ivanfcomesen <ivanfcomesen@gmail.com>
2018-05-06 19:34:28 -0600

    RxJS Part2

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -1,30 +1,42 @@
-import { DishService } from '../services/dish.service';
-import { Params, ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { Component, OnInit} from '@angular/core';
+import {DishService} from '../services/dish.service';
+import {Params, ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
 import {Dish} from '../shared/dish';
 
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-  selector: 'app-dish-detail',
-  templateUrl: './dish-detail.component.html',
-  styleUrls: ['./dish-detail.component.scss']
+    selector: 'app-dish-detail',
+    templateUrl: './dish-detail.component.html',
+    styleUrls: ['./dish-detail.component.scss']
 })
 export class DishDetailComponent implements OnInit {
-  
-  dish: Dish;
-  
- constructor(private dishservice: DishService,private route: ActivatedRoute,
- private location: Location) { }
 
-  ngOnInit() {
-    let id = +this.route.snapshot.params['id'];
-      this.dishservice.getDish(id).subscribe(dish => this.dish =dish);
-  }
-  
+    dishIds: number[];
+    prev: number;
+    next: number;
+    dish: Dish;
+
+    constructor(private dishservice: DishService, private route: ActivatedRoute,
+        private location: Location) {}
+
+    ngOnInit() {
+      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    }
+
+    setPrevNext(dishId: number) {
+        let index = this.dishIds.indexOf(dishId);
+        this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+        this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+    }
+
     goBack(): void {
-    this.location.back();
-  }  
+        this.location.back();
+    }
 
 }
 // ng generate module app-routing --module app.module.ts
